bd74334d4b5c1925e7c90974abd789cd5f52f0eb
João Victor Andrade de Aquino Saraiva <64429904+saraiva142@users.noreply.github.com>
2022-06-25 21:01:53 -0300

    Commit site
    Commit inicial

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== POPULAR SWIPER ===============*/
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            spaceBetween: 48,
+        },
+    },
+});
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+
+/* Link active featured */
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__title, .popular__container`)
+sr.reveal(`.home__subtitle`, { delay: 500 })
+sr.reveal(`.home__elec`, { delay: 600 })
+sr.reveal(`.home__img`, { delay: 800 })
+sr.reveal(`.home__car-data`, { delay: 900, interval: 100, origin: 'bottom' })
+sr.reveal(`.home__button`, { delay: 1000, origin: 'bottom' })
+sr.reveal(`.about__group`, { origin: 'left' })
+sr.reveal(`.about__data`, { origin: 'right' })
